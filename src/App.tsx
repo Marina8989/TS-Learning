cb3f50736d9d7bf7ac1e90b5dd9b4164f06c6e72
Marina8989 <marina.mill209@gmail.com>
2022-01-20 12:29:18 -0800

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, createContext } from 'react';
 import './App.css';
 import { Person, HairColor } from "./components/Person";
 
+interface AppContextInterface{
+    name: string;
+    age: number;
+    country: string
+}
+
+const AppContext = createContext<AppContextInterface | null>(null);
+
 const App: FC = () => {
+
+  const contectValue: AppContextInterface = {
+    name: "Pedro",
+    age: 20,
+    country: "Brazil"
+  }
   return (
+    <AppContext.Provider value={contectValue} >
     <div className="App">
        <Person name="Pedro" age={20} email="pedro@gmail.com" hairColor={HairColor.Pink}/>
     </div>
+    </AppContext.Provider>
   );
 }
 
