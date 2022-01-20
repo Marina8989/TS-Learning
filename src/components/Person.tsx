f887c03681f77a3a7e03c40602e25d65d48412d9
Marina8989 <marina.mill209@gmail.com>
2022-01-20 12:15:01 -0800

    ts

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import { FC } React from 'react';
+import { FC,ChangeEvent, useState } from 'react';
 
 interface Props {
    name: string;
@@ -7,13 +7,22 @@ interface Props {
 }
 
 
+export const Person: FC<Props> = ({name, age, email})  => {
 
-export const Person: FC  = ({name, email, age}: Props)  => {
+    const [country, setCountry] = useState<string | null>("");
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+       setCountry(event.target.value)
+    }
+
   return (
     <div>
       <h1>{name}</h1>
       <h1>{age}</h1>
       <h1>{email}</h1>
+      <input placeholder="Write down your country..." onChange={handleChange}/>
+
+      {country}
     </div>
   );
 }
